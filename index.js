ab7d26fd648eee5c39b14a84ce0695caac07fb8b
Kashingi <moriskashing74@gmail.com>
2026-01-11 18:51:58 +0300

    Added control flow content

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 function showMessage() {
-        let number;
-        number = 1234;
-        alert(number)
+        // If conditions
+        // if (grade === "A") {
+        //         console.log("Excellent!");
+        // } else if (grade === "B") {
+        //         console.log("Good job!");
+        // } else if (grade === "C") {
+        //         console.log("Fair.");
+        // } else if (grade === "D") {
+        //         console.log("Needs improvement.");
+        // } else {
+        //         console.log("Invalid grade.");
+        // }
+        //Switch case
+        // let grade = "B";
+
+        // switch (grade) {
+        //         case "A":
+        //                 console.log("Excellent!");
+        //                 break;
+        //         case "B":
+        //                 console.log("Good job!");
+        //                 break;
+        //         case "C":
+        //                 console.log("Fair.");
+        //                 break;
+        //         case "D":
+        //                 console.log("Needs improvement.");
+        //                 break;
+        //         default:
+        //                 console.log("Invalid grade.");
+        // }
+        //For loop
+        for (let number = 0; number < 5; number++) {
+                console.log(number)
+                
+        }
+        //While loop
+        // let number = 0;
+        // while (number < 5) {
+        //         console.log(number)
+        //         number ++;
+        // }
+        //do while loop
+        do while (condition) {
+                
+        }
+
+
 }
+//Functions
+let age = 51;
+function changeText() {
+        document.getElementById("topic").innerHTML = "Js In web development";
+}
+
+function calculateSum(sum, sub) {
+        let number = 24;
+        let number1 = 36;
+        sum = number + number1;
+        sub = number1 - number;
+        console.log('Results of the sumation is : ', sum)
+}
+calculateSum();//Functions call
